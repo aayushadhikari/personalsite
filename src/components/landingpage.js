abee61abce910fc9542a6016598027d7e294bd55
Aayush Adhikari <aayushadhikari11@gmail.com>
2020-07-25 19:27:59 -0400

    carousel

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -13,90 +13,94 @@ class LandingPage extends Component {
           <Cell col={12}>
             <img src={avatar} alt="avatar" className="avatar-image" />
 
-            <Slider>
-              <div className="banner-text">
-                <h1 style={{ fontFamily: "'Monoton', cursive" }}>
-                  {"Software Engineer"}
-                </h1>
-                <hr />
-                <p>
-                  Web Development | Machine Learning | BlockChain | Internet
-                </p>
-                <div className="social-links">
-                  <a
-                    rel="noopener noreferrer"
-                    href="https://www.linkedin.com/in/aayush-adhikari-702707148/"
-                    target="_blank"
-                  >
-                    <i className="fa fa-linkedin" aria-hidden="true" />
-                  </a>
-                  <a
-                    rel="noopener noreferrer"
-                    href="https://www.facebook.com/aayush.adhikari17"
-                    target="_blank"
-                  >
-                    <i className="fa fa-facebook" aria-hidden="true" />
-                  </a>
-                  <a
-                    rel="noopener noreferrer"
-                    href="https://github.com/aayushadhikari"
-                    target="_blank"
-                  >
-                    <i className="fa fa-github" aria-hidden="true" />
-                  </a>
-                  <a
-                    rel="noopener noreferrer"
-                    href="https://www.instagram.com/aayush_adhikari_/?hl=en"
-                    target="_blank"
-                  >
-                    <i className="fa fa-instagram" aria-hidden="true" />
-                  </a>
+            <Carousel>
+              <Carousel.Item>
+                <div className="banner-text">
+                  <h1 style={{ fontFamily: "'Monoton', cursive" }}>
+                    {"Software Engineer"}
+                  </h1>
+                  <hr />
+                  <p>
+                    Web Development | Machine Learning | BlockChain | Internet
+                  </p>
+                  <div className="social-links">
+                    <a
+                      rel="noopener noreferrer"
+                      href="https://www.linkedin.com/in/aayush-adhikari-702707148/"
+                      target="_blank"
+                    >
+                      <i className="fa fa-linkedin" aria-hidden="true" />
+                    </a>
+                    <a
+                      rel="noopener noreferrer"
+                      href="https://www.facebook.com/aayush.adhikari17"
+                      target="_blank"
+                    >
+                      <i className="fa fa-facebook" aria-hidden="true" />
+                    </a>
+                    <a
+                      rel="noopener noreferrer"
+                      href="https://github.com/aayushadhikari"
+                      target="_blank"
+                    >
+                      <i className="fa fa-github" aria-hidden="true" />
+                    </a>
+                    <a
+                      rel="noopener noreferrer"
+                      href="https://www.instagram.com/aayush_adhikari_/?hl=en"
+                      target="_blank"
+                    >
+                      <i className="fa fa-instagram" aria-hidden="true" />
+                    </a>
+                  </div>
                 </div>
-              </div>
+              </Carousel.Item>
 
-              <div className="banner-text">
-                <h1 style={{ fontFamily: "'Monoton', cursive" }}>
-                  {"Made With Love"}
-                </h1>
-                <hr />
-                <p>
-                  {" "}
-                  And With
-                  <br />
-                  HTML/CSS | Bootstrap | JavaScript | React
-                </p>
-                <div className="social-links">
-                  <a
-                    rel="noopener noreferrer"
-                    href="https://www.linkedin.com/in/aayush-adhikari-702707148/"
-                    target="_blank"
-                  >
-                    <i className="fa fa-linkedin" aria-hidden="true" />
-                  </a>
-                  <a
-                    rel="noopener noreferrer"
-                    href="https://www.facebook.com/aayush.adhikari17"
-                    target="_blank"
-                  >
-                    <i className="fa fa-facebook" aria-hidden="true" />
-                  </a>
-                  <a
-                    rel="noopener noreferrer"
-                    href="https://github.com/aayushadhikari"
-                    target="_blank"
-                  >
-                    <i className="fa fa-github" aria-hidden="true" />
-                  </a>
-                  <a
-                    rel="noopener noreferrer"
-                    href="https://www.instagram.com/aayush_adhikari_/?hl=en"
-                    target="_blank"
-                  >
-                    <i className="fa fa-instagram" aria-hidden="true" />
-                  </a>
+              <Carousel.Item>
+                <div className="banner-text">
+                  <h1 style={{ fontFamily: "'Monoton', cursive" }}>
+                    {"Made With Love"}
+                  </h1>
+                  <hr />
+                  <p>
+                    {" "}
+                    And With
+                    <br />
+                    HTML/CSS | Bootstrap | JavaScript | React
+                  </p>
+                  <div className="social-links">
+                    <a
+                      rel="noopener noreferrer"
+                      href="https://www.linkedin.com/in/aayush-adhikari-702707148/"
+                      target="_blank"
+                    >
+                      <i className="fa fa-linkedin" aria-hidden="true" />
+                    </a>
+                    <a
+                      rel="noopener noreferrer"
+                      href="https://www.facebook.com/aayush.adhikari17"
+                      target="_blank"
+                    >
+                      <i className="fa fa-facebook" aria-hidden="true" />
+                    </a>
+                    <a
+                      rel="noopener noreferrer"
+                      href="https://github.com/aayushadhikari"
+                      target="_blank"
+                    >
+                      <i className="fa fa-github" aria-hidden="true" />
+                    </a>
+                    <a
+                      rel="noopener noreferrer"
+                      href="https://www.instagram.com/aayush_adhikari_/?hl=en"
+                      target="_blank"
+                    >
+                      <i className="fa fa-instagram" aria-hidden="true" />
+                    </a>
+                  </div>
                 </div>
-              </div>
-            </Slider>
+              </Carousel.Item>
+            </Carousel>
           </Cell>
         </Grid>
       </div>
