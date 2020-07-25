0dd33ec9bf49de97e518a0233cb980417ace57b0
Aayush Adhikari <aayushadhikari11@gmail.com>
2020-07-25 16:39:34 -0400

    web

--- src/App.js
+++ src/App.js
@@ -1,37 +1,42 @@
-import React from 'react';
-import './App.css';
-import {Layout,Navigation,Drawer,Header,Content} from 'react-mdl';
-import Main from './components/main';
-import {Link} from 'react-router-dom';
+import React from "react";
+import "./App.css";
+import { Layout, Navigation, Drawer, Header, Content } from "react-mdl";
+import Main from "./components/main";
+import { Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="demo-big-content">
-    <Layout>
-        <Header className="header-color" title="Aayush Adhikari" scroll>
-            <Navigation>
-                <Link to = "/">Home</Link>
-                <Link to="/resume">Resume</Link>
-                <Link to="/projects">Projects</Link>
-                <Link to="/aboutme">About Me</Link>
-                <Link to="/contact">Contact</Link>
-            </Navigation>
+      <Layout>
+        <Header
+          transparent
+          className="header-color"
+          title="Aayush Adhikari"
+          scroll
+        >
+          <Navigation>
+            <Link to="/">Home</Link>
+            <Link to="/resume">Resume</Link>
+            <Link to="/projects">Projects</Link>
+            <Link to="/aboutme">About Me</Link>
+            <Link to="/contact">Contact</Link>
+          </Navigation>
         </Header>
         <Drawer title="Title">
-            <Navigation>
-                <a href="#">Link</a>
-                <a href="#">Link</a>
-                <a href="#">Link</a>
-                <a href="#">Link</a>
-            </Navigation>
+          <Navigation>
+            <a href="#">Link</a>
+            <a href="#">Link</a>
+            <a href="#">Link</a>
+            <a href="#">Link</a>
+          </Navigation>
         </Drawer>
         <Content>
-            <div className="page-content">
-            <Main/>
-            </div>
+          <div className="page-content">
+            <Main />
+          </div>
         </Content>
-    </Layout>
-</div>
+      </Layout>
+    </div>
   );
 }
 
