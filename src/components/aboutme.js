5fa29998dcb245990b737159456601e336d30acd
Aayush Adhikari <aayushadhikari11@gmail.com>
2020-07-25 21:42:14 -0400

    cleanup

--- src/components/aboutme.js
+++ src/components/aboutme.js
@@ -1,13 +1,9 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class AboutMe extends Component {
-    render() {
-        return(
-            <div>
-                <h1>About Page</h1>
-            </div>
-        )
-    }
+  render() {
+    return <div></div>;
+  }
 }
 
 export default AboutMe;
--- src/components/resume.js
+++ src/components/resume.js
@@ -1,13 +1,9 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Resume extends Component {
-    render() {
-        return(
-            <div>
-                <h1>Resume Page</h1>
-            </div>
-        )
-    }
+  render() {
+    return <div></div>;
+  }
 }
 
 export default Resume;
