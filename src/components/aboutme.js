a37c99370ecaa6016437f3fe50fc34394fabf318
Aayush Adhikari <aayushadhikari11@gmail.com>
2020-07-26 00:02:23 -0400

    shot

--- src/components/aboutme.js
+++ src/components/aboutme.js
@@ -5,10 +5,6 @@ class AboutMe extends Component {
     return (
       <div className="container about">
         <h1>What do I do? I drink and I know things</h1>
-        <h2>
-          Beaming curiosity is what keeps us alive, and I strive to keep the
-          curiosity flowing, that's what will save us in the end.
-        </h2>
       </div>
     );
   }
