936630b23bee02a7e6a62e3941ca00f5bed579ff
Aayush Adhikari <aayushadhikari11@gmail.com>
2020-07-26 19:05:05 -0400

    noadmin

--- src/components/main.js
+++ src/components/main.js
@@ -1,25 +1,19 @@
-import React from 'react';
-import { Switch, Route } from 'react-router-dom';
-import LandingPage from './landingpage';
-import AboutMe from './aboutme';
-import Contact from './contact';
-import Resume from './resume';
-import Projects from './projects';
-import AdminLogin from './AdminLogin';
-import AdminPage from './AdminPage';
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import LandingPage from "./landingpage";
+import AboutMe from "./aboutme";
+import Contact from "./contact";
+import Resume from "./resume";
+import Projects from "./projects";
 
 const Main = () => (
-
-    <Switch>
-        <Route exact path="/" component={LandingPage}/>
-        <Route exact path="/aboutme" component={AboutMe}/>
-        <Route exact path="/contact" component={Contact}/>
-        <Route exact path="/resume" component={Resume}/>
-        <Route exact path="/projects" component={Projects}/>
-        <Route exact path="/adminlogin" component={AdminLogin} />
-        <Route exact path="/adminpage" component={AdminPage} />
-    </Switch>
-)
-
+  <Switch>
+    <Route exact path="/" component={LandingPage} />
+    <Route exact path="/aboutme" component={AboutMe} />
+    <Route exact path="/contact" component={Contact} />
+    <Route exact path="/resume" component={Resume} />
+    <Route exact path="/projects" component={Projects} />
+  </Switch>
+);
 
 export default Main;
