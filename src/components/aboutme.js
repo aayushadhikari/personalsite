6d3083928f3ad2282dbd00a848c6284ef7349392
Aayush Adhikari <aayushadhikari11@gmail.com>
2020-07-28 20:48:22 -0400

    info

--- src/components/aboutme.js
+++ src/components/aboutme.js
@@ -4,7 +4,7 @@ class AboutMe extends Component {
   render() {
     return (
       <div className="container about">
-        <h1>What do I do? I drink and I know things</h1>
+        <h1>Infomation Coming Soon ...</h1>
       </div>
     );
   }
