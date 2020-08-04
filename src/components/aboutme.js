dab38ed10ec9f44cbc349fc41cfa34be659d1a35
Aayush Adhikari <aayushadhikari11@gmail.com>
2020-08-04 19:50:03 -0400

    more info

--- src/components/aboutme.js
+++ src/components/aboutme.js
@@ -4,10 +4,12 @@ class AboutMe extends Component {
   render() {
     return (
       <div className="container about">
-        <h1>
+        <h1>This is Me</h1>
+        <p>
           An aspiring software engineer with great love for coding and playing
           music.
-        </h1>
+        </p>
+        <p>Just like anyone else, I am in search for a why.</p>
       </div>
     );
   }
